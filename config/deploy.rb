9d04a6f7661bcf79e03d34bc055df7942feaaa06
Travis Roberts <traviskroberts@gmail.com>
2009-06-14 12:22:36 -0500

    capify

---
+++ config/deploy.rb
@@ -0,0 +1,62 @@
+require 'railsmachine/recipes'
+
+# =============================================================================
+# REQUIRED VARIABLES
+# =============================================================================
+# The name of your application. Used for directory and file names
+set :application, "doingwrong"
+
+# Target directory for the application on the web and app servers.
+set :deploy_to, "/var/www/apps/#{application}"
+
+# Primary domain name of your application. Used as a default for all server roles.
+set :domain, "thisiswhatyouredoingwrong.com"
+
+# Login user for ssh.
+set :user, "deploy"
+set :runner, user
+set :admin_runner, user
+
+# Rails environment. Used by application setup tasks and migrate tasks.
+set :rails_env, "production"
+
+# =============================================================================
+# ROLES
+# =============================================================================
+# Modify these values to execute tasks on a different server.
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
+role :scm, domain
+
+# =============================================================================
+# APPLICATION SERVER OPTIONS
+# ============================================================================= 
+set :app_server, :passenger  # :mongrel or :passenger
+
+# =============================================================================
+# DATABASE OPTIONS
+# =============================================================================
+set :database, :mysql   # mysql or postgresql
+
+# =============================================================================
+# SCM OPTIONS
+# =============================================================================
+set :scm, :git
+set :repository, "git@github.com:travisr/#{application}.git"
+
+# =============================================================================
+# CAPISTRANO OPTIONS
+# =============================================================================
+set :keep_releases, 5
+set :deploy_via, :remote_cache
+
+# action to symlink database file
+namespace :deploy do
+  desc "Symlink database config file."
+  task :symlink_db do
+    run "ln -nfs #{shared_path}/system/database.yml #{release_path}/config/database.yml"
+  end
+end
+ 
+after 'deploy:update_code', 'deploy:symlink_db'
